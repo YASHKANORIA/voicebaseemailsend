5af2ce7c699283c21234c437ee9610b265560c46
YASHKANORIA <45586979+YASHKANORIA@users.noreply.github.com>
2020-12-28 15:04:15 +0530

    first commit

---
+++ emailbot.py
@@ -0,0 +1,56 @@
+import smtplib as smtp
+import speech_recognition as sr
+import pyttsx3
+
+email_list = {
+    'anjali' :'anjalirathi94@gmail.com',
+    'yash' : 'yashbattu@gmail.com',
+    'shubham':'ksubham.sharma@gmail.com',
+    'mohit' :'yshkanoria@hotmail.com'
+}
+print(email_list)
+engine = pyttsx3.init()
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+listener = sr.Recognizer()
+
+def email_information():
+    try:
+        with sr.Microphone() as source:
+            print("Bol bhai")
+            voice = listener.listen(source)
+            voicetotext=listener.recognize_google(voice)
+            print(voicetotext)
+            return voicetotext.lower()
+
+    except:
+        pass
+        print("unble")
+
+def sendmail(receiver,body):
+
+    server = smtp.SMTP('smtp.gmail.com' ,   587)
+
+    server.starttls()
+    server.login('workyashkanoria@gmail.com','****************')
+    server.sendmail('workyashkanoria@gmail.com',receiver,body)
+
+def get_info_for_email():
+    talk("Reciver Mail Address :")
+    receiver = email_information()
+    gmail=email_list[receiver]
+    print("-----------------------------------")
+    print(gmail)
+    talk("Subject to be")
+    subject=email_information()
+    talk("message ")
+    message = email_information()
+
+    sendmail(gmail,message)
+
+get_info_for_email()
